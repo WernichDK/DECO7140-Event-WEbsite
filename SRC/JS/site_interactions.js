2fa3cfb327002ff69670db0e51c296889b6a536a
WernichDK <w.dekorte@uqconnect.edu.au>
2024-03-12 10:26:14 +1000

    added weather JS

--- SRC/JS/site_interactions.js
+++ SRC/JS/site_interactions.js
@@ -5,3 +5,35 @@ discount_code = discount_code + 1;
 alert("Your discount code is " + discount_code);
 }
 discount_button.addEventListener("click", showDiscountCode);
+
+// Base URL of the open Mateo API Endpoint
+const baseUrl = 'https://api.open-meteo.com/v1/forecast';
+
+//query parameters as a JS object
+const queryParams = {
+    latitude: -27.4679,
+    longitude: 153.0281,
+    current_weather: true,
+};
+
+//Convert the query parameters object into a query string
+const queryString = new URLSearchParams(queryParams).toString();
+
+// Full URL with query parameters
+// const urlWithParams = '${baseUrl}?${QueryString}';
+const urlWithParams = baseUrl+"?"+queryString;
+
+// Request options 
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+//Making the Fetch call
+fetch(urlWithParams, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        const weather = data.current_weather;
+        console.log("Current temperature: " + weather.temperature + "C");        
+    })
+.catch(error => console.log('error', error));
