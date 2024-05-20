f55ef6ecf86949cc3c4bae04f5f0949425322f48
WernichDK <w.dekorte@uqconnect.edu.au>
2024-05-20 16:55:21 +1000

    api continued

--- SRC/website_implementation/js/submitsong.js
+++ SRC/website_implementation/js/submitsong.js
@@ -1,5 +1,8 @@
 const songuplab = document.getElementById('songuplab');
 const audio_file = document.getElementById('audio_file');
+const my_website_code = "wen123";
+const baseURL = "https://damp-castle-86239-1b70ee448fbd.herokuapp.com/decoapi/musicians/";
+const postMusicAudioMethod = "POST";
 
 const triggerFileInput = () => {
     audio_file.click();
@@ -14,6 +17,69 @@ const handleFileChange = () => {
     songuplab.textContent = fileName;
 };
 
+
+
+const handleFormSubmit = event => {
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+    formData.append("website_code", my_website_code);
+
+    const requestOptions = {
+        method: postMusicAudioMethod,
+        body: formData,
+        redirect: 'follow'
+    }
+
+    fetch(baseURL, requestOptions)
+    .then(response => response.json().then(data => {
+        if (!response.ok) {
+            console.log("Server response:", data);
+            throw new Error("Network response was not ok");
+        }
+        return data;
+    }))
+    .then(data => {
+        console.log(data.description);
+        submitSong.reset();
+        songuplab.textContent = "Audio File:"
+        alert('Your song "${data.description}" was submitted successfully!');
+    })
+    .catch(error => {
+        console.error("There was a problem with the fetch operation:", error.message);
+        alert("Error submitting song. Please try again.");
+    });
+
+
+    
+};
+
+
 songuplab.addEventListener('click', triggerFileInput);
 audio_file.addEventListener('change', handleFileChange);
+submitSong.addEventListener("submit", handleFormSubmit);
 
+const getUploadedSongs = () => {
+    const queryParams = {
+        website_code: my_website_code,
+    }
+    const queryString = new URLSearchParams(queryParams).toString();
+    const urlWithParams = baseURL+"?"+queryString;
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    }
+
+    fetch(urlWithParams, requestOptions)
+    .then(response => {
+        if (!response.ok) {
+                throw new Error("Network response was not ok");
+        }
+        return response.json();
+    })
+    .then(song => console.log(song))
+    .catch(error => {
+        console.error("Error processing song:", error.message);
+        alert("There was a problem loading events. Please referesh the page to try again.");
+    });
+};
